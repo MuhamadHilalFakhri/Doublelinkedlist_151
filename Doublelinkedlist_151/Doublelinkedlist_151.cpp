6b31242ba46bfbf6fd59ed6f43b80f9c9a2f3cc2
hilal555 <muhamadhilal04@gmail.com>
2023-05-26 11:02:59 +0700

    menambahkan method

--- Doublelinkedlist_151/Doublelinkedlist_151.cpp
+++ Doublelinkedlist_151/Doublelinkedlist_151.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 class Node {
@@ -13,5 +12,16 @@ public:
 
 class DoubleLinkedList {
 private:
+	Node* START;
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchData();
 
 };
